91ad8151fb946529b4713260012c26274bfef656
papamoon0113 <metube0113@gmail.com>
2023-10-24 18:50:24 +0900

    과제 프로젝트 분리

---
+++ 교육 파일/springedu/src/main/java/com/example/springedu/filter/TestFilter1.java
@@ -0,0 +1,23 @@
+package com.example.springedu.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+
+@Component
+@Slf4j
+@Order(2)
+public class TestFilter1 implements Filter {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+                        throws IOException, ServletException {
+        log.info("[필터1] 요청 자원 수행 전");
+        chain.doFilter(request, response);
+        log.info("[필터1] 요청 자원 수행 후");
+    }
+}
---
+++ 교육 파일/springedu/src/main/java/com/example/springedu/interceptor/TestInterceptor.java
@@ -0,0 +1,28 @@
+package com.example.springedu.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+public class TestInterceptor implements HandlerInterceptor {
+    public  boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        log.info("[인터셉터] preHandle 수행");
+        return true;
+    }
+
+    public void postHandle(
+            HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView)
+            throws Exception {
+        log.info("[인터셉터] postHandle 수행");
+    }
+
+    public void afterCompletion(
+            HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception{
+        log.info("[인터셉터] afterCompletion 수행");
+    }
+}
